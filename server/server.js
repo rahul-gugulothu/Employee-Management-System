8fc67d81329a752b1a2c5bbf3580cfae3c027b1a
rahul-gugulothu <rahulg.321r@gmail.com>
2026-07-01 11:04:58 +0530

    Connect backend to mongoDB atlas

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
 
 // Middleware
@@ -15,6 +17,21 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// Start Server
+const startServer = async () => {
+    try {
+        // Connect to MongoDB first
+        await connectDB();
+
+        // Start Express only after DB is connected
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Failed to start server");
+        process.exit(1);
+    }
+};
+
+startServer();
